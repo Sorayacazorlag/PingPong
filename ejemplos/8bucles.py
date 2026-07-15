07d90997f060a8a4b7b7d568bec3cfe8f01aa850
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-07-15 11:51:39 +0200

    updatepingpong

--- ejemplos/8bucles.py
+++
@@ -1,7 +0,0 @@
-#en range el numero de veces que quieres que se repita. Empezando en 0
-for i in range(5):
-    print("Contador ", i+1)
-
-#Imprime 10 veces la palabra hola
-for i in range(10):
-    print("Hola")
---
+++ numbersandpingpong.py
@@ -0,0 +1,9 @@
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("PingPong")
+    elif i % 3 == 0:
+        print("Ping")
+    elif i % 5 == 0:
+        print("Pong")
+    else:
+        print(i)
